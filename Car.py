e7ec871bd11783c9893d139add40caf09754e58f
Lucas Axel Gamaleri <lucas.gamaleri.92@gmail.com>
2018-12-25 21:29:39 -0300

    Update and rename Car.m to Car.py

---
+++ Car.py
@@ -0,0 +1,20 @@
+##    Car DATA // Parametros masicos y coeficientes	
+
+K1= 14900.0     # Rigidez suspension delantera izquierda N/m
+K2= 14900.0     # Rigidez suspension delantera derecha N/m
+
+KB1= 150000.0     # Rigidez de la cubierta delantera izquierda N/m
+KB2= 150000.0     # Rigidez de la cubierta delantera derecha N/m
+
+m= 2100.0       # Masa suspendida del vehículo N
+m1= 2.0          # Masa de la rueda delantera izquierda N
+m2= 2.0          # Masa de la rueda delantera derecha N
+
+b1= 2000.0       # Amortiguación suspensión delantera izquierda N*s/m
+b2= 2000.0       # Amortiguación suspensión delantera derecha N*s/m
+
+a= 1.2        # Distancia CG al tren delantero
+b= 2.0          # Distancia CG al tren trasero m
+Ix= 614.0        # Momento de inercia respecto a eje x Kg*m²
+Iy= 2760.0       # Momento de inercia respecto a eje y Kg*m²
+W= 1.6        # Distancia entre ejes m
